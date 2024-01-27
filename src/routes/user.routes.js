6813542bb7c73eaa01ea5de5d9b89594141fbbad
Dinesh Ranjan Biswal <124261545+dineshranjan2001@users.noreply.github.com>
2024-01-27 16:03:09 +0530

    router setup for user changeCurrentPassword,getCurrentUser,updateProfile,updateAvatar,updateCoverImage,getProfileDetails and getUserHistory endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { 
+  changeCurrentPassword, 
+  getCurrentUser, 
+  getUserWatchHistory, 
+  loginUser, 
+  logoutUser, 
+  refreshAccessToken, 
+  registerUser, 
+  updateAvatar, 
+  updateCoverImage, 
+  updateProfleDetails 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/fileUpload.middleware.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
@@ -23,5 +34,12 @@ router.route("/login").post(loginUser);
 //secure routes
 router.route("/logout").post(verifyToken,logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-current-password").post(verifyToken,changeCurrentPassword);
+router.route("/get-current-user").get(verifyToken,getCurrentUser);
+router.route("/update-profile").post(verifyToken,updateProfleDetails);
+router.route("/avatar").post(verifyToken,upload.single("avatar"),updateAvatar);
+router.route("/cover-image").post(verifyToken,upload.single("coverImage"),updateCoverImage);
+router.route("/get-profile-details/:username").get(verifyToken,updateProfleDetails);
+router.route("/get-user-history").get(verifyToken,getUserWatchHistory);
 
 export default router;
